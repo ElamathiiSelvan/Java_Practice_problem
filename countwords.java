c8b19594b9adc6789bce3e7c8e13dae3d9cdf1c5
Elamathi Selvan <elamathis8060@gmail.com>
2024-11-11 18:18:24 +0530

    countwords

---
+++ countwords.java
@@ -0,0 +1,10 @@
+public class Count_words {
+
+	public static void main(String[] args) {
+		String str= "hello welcome to java";
+		String[] word = str.split(" ");
+		System.out.println(word.length);
+
+	}
+
+}
