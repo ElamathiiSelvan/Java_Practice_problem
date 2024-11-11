8c486f44511691848e83927109a42d585721a9c7
Elamathi Selvan <elamathis8060@gmail.com>
2024-11-11 18:42:03 +0530

    StringCase

---
+++ StringConverter.java
@@ -0,0 +1,11 @@
+public class StringCase {
+    public static void main(String[] args) {
+        String input = "Python";
+        
+        String upperCase = input.toUpperCase();
+        String lowerCase = input.toLowerCase();
+        
+        System.out.println("Uppercase: " + upperCase);
+        System.out.println("Lowercase: " + lowerCase);
+    }
+}
