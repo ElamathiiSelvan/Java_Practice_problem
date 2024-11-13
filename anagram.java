337c836e9e8fd52327fdcbd64489318a02e20d14
Elamathi Selvan <elamathis8060@gmail.com>
2024-11-13 21:26:13 +0530

    anagram.java

---
+++ anagram.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        
+        if (Arrays.equals(charArray1, charArray2)) {
+            System.out.println("It is an Anagrams");
+        } else {
+            System.out.println("It is not Anagrams");
+        }
+    }
+}
