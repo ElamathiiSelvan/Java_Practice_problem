c769515e62bc75272f117d8b99d3c4cfa1582f6d
Elamathi Selvan <elamathis8060@gmail.com>
2024-11-13 20:58:49 +0530

    ReplaceSpaceHypen

---
+++ ReplaceSpaceHypen.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String input = "Hello World";
+        String result = input.replace(" ", "-");
+        System.out.println("Output: " + result);
+    }
+}
